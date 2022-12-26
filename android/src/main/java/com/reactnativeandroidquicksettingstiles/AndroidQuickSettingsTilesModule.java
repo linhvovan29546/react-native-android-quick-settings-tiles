857d215d74dbd0f0db72b0c330914003c6230e86
Linh Vo <vovanlinh29546@gmail.com>
2022-12-26 16:29:45 +0700

    feat: init function request permisson

--- android/src/main/java/com/reactnativeandroidquicksettingstiles/AndroidQuickSettingsTilesModule.java
+++ android/src/main/java/com/reactnativeandroidquicksettingstiles/AndroidQuickSettingsTilesModule.java
@@ -1,21 +1,35 @@
 package com.reactnativeandroidquicksettingstiles;
 
-import androidx.annotation.NonNull;
+import android.annotation.SuppressLint;
+import android.app.StatusBarManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.core.graphics.drawable.IconCompat;
+
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
+import com.google.common.util.concurrent.MoreExecutors;
 
 @ReactModule(name = AndroidQuickSettingsTilesModule.NAME)
 public class AndroidQuickSettingsTilesModule extends ReactContextBaseJavaModule {
     public static final String NAME = "AndroidQuickSettingsTiles";
-
+    public static ReactApplicationContext context;
     public AndroidQuickSettingsTilesModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        context = reactContext;
     }
-
+    private Context getAppContext() {
+      return this.context.getApplicationContext();
+    }
     @Override
     @NonNull
     public String getName() {
@@ -25,10 +39,46 @@ public class AndroidQuickSettingsTilesModule extends ReactContextBaseJavaModule 
 
     // Example method
     // See https://reactnative.dev/docs/native-modules-android
+    @SuppressLint("LongLogTag")
     @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    public void requestPermission(Promise promise) {
+      Log.d(NAME, "start native");
+      WritableMap params = Arguments.createMap();
+      StatusBarManager statusBarService = null;
+      Log.d(NAME, "11111");
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+        Context context=getAppContext();
+        statusBarService = context.getSystemService(StatusBarManager.class);
+        if (Build.VERSION.SDK_INT == 33) {
+          ComponentName componentName = new ComponentName(
+            context,
+            "Test App");
+          IconCompat icon =
+            IconCompat.createWithResource(context,
+              this.getResourceIdForResourceName(context,"ic_launcher_round"));
+          statusBarService.requestAddTileService(
+            componentName, "Quick Settings",icon.toIcon(context) ,
+            MoreExecutors.directExecutor(), integer -> {
+              params.putInt("integer",integer);
+              promise.resolve(params);
+            });
+        } else {
+          params.putString("message","Request to add tile for user is not supported");
+          promise.reject("error",params);
+        }
+      }else{
+        params.putString("message","Request to add tile for user is not supported");
+        promise.reject("error",params);
+      }
+
+
     }
 
-    public static native int nativeMultiply(int a, int b);
+  private int getResourceIdForResourceName(Context context, String resourceName) {
+    int resourceId = context.getResources().getIdentifier(resourceName, "drawable", context.getPackageName());
+    if (resourceId == 0) {
+      resourceId = context.getResources().getIdentifier(resourceName, "mipmap", context.getPackageName());
+    }
+    return resourceId;
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
-import { NativeModules } from 'react-native';
-
-type AndroidQuickSettingsTilesType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
+import { NativeModules, Platform } from 'react-native';
+const LINKING_ERROR =
+  `The package 'react-native-android-quick-settings-tiles' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
 const { AndroidQuickSettingsTiles } = NativeModules;
-
-export default AndroidQuickSettingsTiles as AndroidQuickSettingsTilesType;
+const quickSettings = AndroidQuickSettingsTiles
+  ? AndroidQuickSettingsTiles
+  : new Proxy(
+    {},
+    {
+      get() {
+        throw new Error(LINKING_ERROR);
+      },
+    }
+  );
+class RNQuickSettings {
+  requestPermission = (): Promise<any> => {
+    console.log('start js native module0')
+    return quickSettings.requestPermission()
+  }
+}
+export default new RNQuickSettings();
