b1704ec4081a6e81339920972c0eb0594290c02e
Linh Vo <vovanlinh29546@gmail.com>
2022-12-26 16:50:44 +0700

    feat: init feature request permission

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
 import { NativeModules, Platform } from 'react-native';
-
 const LINKING_ERROR =
   `The package 'react-native-android-quick-settings-tiles' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
-
-const AndroidQuickSettingsTiles = NativeModules.AndroidQuickSettingsTiles
-  ? NativeModules.AndroidQuickSettingsTiles
+const { AndroidQuickSettingsTiles } = NativeModules;
+const quickSettings = AndroidQuickSettingsTiles
+  ? AndroidQuickSettingsTiles
   : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export function multiply(a: number, b: number): Promise<number> {
-  return AndroidQuickSettingsTiles.multiply(a, b);
+    {},
+    {
+      get() {
+        throw new Error(LINKING_ERROR);
+      },
+    }
+  );
+class RNQuickSettings {
+  requestPermission = (): Promise<any> => {
+    console.log('start js native module0')
+    return quickSettings.requestPermission()
+  }
 }
+export default new RNQuickSettings();
